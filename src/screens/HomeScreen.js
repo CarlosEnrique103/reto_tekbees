9bbe4239180f439efbe776982876b14a584317a3
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2023-10-06 04:40:35 -0500

    feat: add animation to homeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../constants/colors";
+import LottieView from "lottie-react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.root}>
-      <Text>HomeScreen</Text>
+      <LottieView
+        autoPlay
+        style={styles.animation}
+        source={require("./../../assets/animations/welcome.json")}
+      />
     </View>
   );
 };
